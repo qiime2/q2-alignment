d715e6c746e267f0b1a4441894f96c8b2707b422
Greg Caporaso <gregcaporaso@gmail.com>
2016-08-05 10:54:19 -0700

    TST: adds travis, flake8 fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     version="0.0.0-dev",
     packages=find_packages(),
     install_requires=['scikit-bio', 'qiime >= 2.0.0', 'q2-types'],
-    #package_data={'q2_alignment': ['markdown/*md']},
+    # package_data={'q2_alignment': ['markdown/*md']},
     author="Greg Caporaso",
     author_email="gregcaporaso@gmail.com",
     description="Create and work with alignments in QIIME 2.",
