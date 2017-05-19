ddf8e5459debe8c46a593f4df2f06d2eb3c63a30
Greg Caporaso <gregcaporaso@gmail.com>
2017-05-19 12:29:35 -0700

    squash me

--- q2_alignment/plugin_setup.py
+++ q2_alignment/plugin_setup.py
@@ -21,7 +21,7 @@ plugin = Plugin(
                    "Standley DM. Mol Biol Evol. 2013 Apr;30(4):772-80. "
                    "doi: 10.1093/molbev/mst010."),
     description=('This QIIME 2 plugin provides support for generating '
-                 'and working with sequence alignments.'),
+                 'and manipulating sequence alignments.'),
     short_description='Plugin for generating and manipulating alignments.'
 )
 
