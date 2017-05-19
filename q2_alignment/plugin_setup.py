1cba9bc7ed6463c629b2540159f0caa91dbde300
Greg Caporaso <gregcaporaso@gmail.com>
2017-05-19 09:19:51 -0700

    DOC: add plugin description

--- q2_alignment/plugin_setup.py
+++ q2_alignment/plugin_setup.py
@@ -19,7 +19,10 @@ plugin = Plugin(
     citation_text=("MAFFT multiple sequence alignment software version 7: "
                    "improvements in performance and usability. Katoh K, "
                    "Standley DM. Mol Biol Evol. 2013 Apr;30(4):772-80. "
-                   "doi: 10.1093/molbev/mst010.")
+                   "doi: 10.1093/molbev/mst010."),
+    description=('This QIIME 2 plugin provides support for generating '
+                 'and working with sequence alignments.'),
+    short_description='Plugin for generating and working with alignments.'
 )
 
 plugin.methods.register_function(
